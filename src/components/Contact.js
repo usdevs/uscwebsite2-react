47406a57ce908aa290b692c7312fc93aa03339a1
Huzaifa Raghav <windowpane1712@gmail.com>
2021-12-30 13:50:33 +0800

    Add header img to Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Navigation from './Navigation';
+import { Container } from 'react-bootstrap';
 
 function Contact() {
     return (
-        <div>
-            <h1> Contact </h1>
-        </div>
+        <>
+            <Navigation></Navigation>
+            <Container>
+                <img 
+                  src='/img/contact-us-header.jpg' 
+                  class="img-fluid" 
+                  style={{maxWidth:"85%"}}
+                  alt="Cinnamon College against a blue sky"></img>
+                <h1> Contact Us </h1>
+            </Container>
+        </>
     )
 }
 
