812ca7afb92095f034a9dd8e7c0d96445632bc88
Huzaifa Raghav <windowpane1712@gmail.com>
2021-12-30 14:12:38 +0800

    Add styling for image and Contact Us h1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import Navigation from './Navigation';
 import { Container } from 'react-bootstrap';
+import { Row,Col } from 'react-bootstrap';
 
 function Contact() {
     return (
         <>
-            <Navigation></Navigation>
-            <Container>
-                <img 
-                  src='/img/contact-us-header.jpg' 
-                  class="img-fluid" 
-                  style={{maxWidth:"85%"}}
-                  alt="Cinnamon College against a blue sky"></img>
-                <h1> Contact Us </h1>
-            </Container>
+        <Navigation></Navigation>
+        <Container style={{paddingLeft:"6rem", paddingRight:"6rem"}}>
+            <img
+                src='/img/contact-us-header.jpg' 
+                className="img-fluid" 
+                alt="Cinnamon College against a blue sky"></img>
+                        
+
+            <h1 class="text-start display-4 mt-3"> Contact Us </h1>
+        </Container>
         </>
     )
 }
